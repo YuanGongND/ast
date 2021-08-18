bbb4dd50d9e4eabed2f07f6e1c884b0456213021
ygong <yuangongfdu@gmail.com>
2021-08-18 09:28:55 -0400

    fix audioset pretrained model path

--- src/models/ast_models.py
+++ src/models/ast_models.py
@@ -123,7 +123,7 @@ class ASTModel(nn.Module):
                 # this model performs 0.4593 mAP on the audioset eval set
                 audioset_mdl_url = 'https://www.dropbox.com/s/cv4knew8mvbrnvq/audioset_0.4593.pth?dl=1'
                 wget.download(audioset_mdl_url, out='../../pretrained_models/audioset_10_10_0.4593.pth')
-            sd = torch.load('../../pretrained_models/ast_audioset.pth', map_location=device)
+            sd = torch.load('../../pretrained_models/audioset_10_10_0.4593.pth', map_location=device)
             audio_model = ASTModel(label_dim=527, fstride=10, tstride=10, input_fdim=128, input_tdim=1024, imagenet_pretrain=False, audioset_pretrain=False, model_size='base384', verbose=False)
             audio_model = torch.nn.DataParallel(audio_model)
             audio_model.load_state_dict(sd, strict=False)
